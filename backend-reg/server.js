e7d930a1e816428f08af051ba81f58c97c57b28d
chisouche <jenniferuc.ju@gmail.com>
2024-06-11 11:46:59 +0100

    Configuring Environment-Specific Settings

--- backend-reg/server.js
+++ backend-reg/server.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const userRoutes = require('./routes/authRoutes');
 const path = require('path');
 
-// Explicitly specify the path to the .env file based on NODE_ENV
-const envPath = path.resolve(__dirname, `.env.${process.env.NODE_ENV || 'development'}`);
+// Determine the environment and set the corresponding .env file
+const env = process.env.NODE_ENV || 'development';
+const envPath = path.resolve(__dirname, `.env.${env}`);
+console.log('Loading environment variables from:', envPath); // Log the path
+
 require('dotenv').config({ path: envPath });
 
 console.log('Loaded Environment Variables from:', envPath); // Log the path
@@ -24,16 +27,14 @@ app.use(express.json());
 
 app.use('/api/auth', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
